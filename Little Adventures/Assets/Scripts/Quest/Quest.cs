9a41c44f446f6f8315c2c028cfa51db2cad7eeb0
MarekChrappa <chrappamarek@gmail.com>
2024-12-20 14:03:52 +0100

    Created quest 2

--- Little Adventures/Assets/Scripts/Quest/Quest.cs
+++ Little Adventures/Assets/Scripts/Quest/Quest.cs
@@ -39,10 +39,7 @@ public class Quest : MonoBehaviour
 
     void Update()
     {
-        if(cutscene_show && Input.anyKeyDown){
-            cutscene_show = false;
-            show_CutScene_text.gameObject.SetActive(false);
-        }
+
     }
 
     public void Show_Button(){
@@ -64,7 +61,7 @@ public class Quest : MonoBehaviour
         interact = false;
         show_text.text = description;
 
-        cutscene_Show();
+        cutscene_Show(cutscene);
         Hide_Button();
         Hide_Quest_Mark();
 
@@ -80,12 +77,17 @@ public class Quest : MonoBehaviour
         show_CutScene_text.text = cutscene;
     }
 
+    public void cutscene_Show(string A){
+        cutscene_show = true;
+        StartCoroutine(ShowMessage(A, 1));
+    }
+
     public void Update_quest(string upd){
         show_text.text = upd;
     }
 
     public void Finish_quest(){
-        StartCoroutine(ShowMessage(EndMessage, 1));
+        StartCoroutine(Show_Last_Message(EndMessage, 3));
         show_text.gameObject.SetActive(false);
         
     }
@@ -98,109 +100,21 @@ public class Quest : MonoBehaviour
 
         // Wait for the specified duration
         yield return new WaitForSeconds(duration);
-        finished = true;
+        //finished = true;
         // Hide the text after the duration
         show_CutScene_text.gameObject.SetActive(false);
     }
 
-
-    /*
-    void Start()
+    IEnumerator Show_Last_Message(string message, float duration)
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        // Set the text and make it visible
+        show_CutScene_text.text = message;
+        show_CutScene_text.gameObject.SetActive(true);
 
-        button.enabled = false;
-        quest_mark.enabled = true;
-        Hide_at_start();
-    }
-
-    void Hide_at_start()
-    {
-        for (int i = 0; i < questList.Count; i++) { 
-            questList[i].enabled = false;
-        }
-    }
-
-    // Update is called once per frame
-
-    public void Show_Button(){
-        button.enabled = true;
-        show = true;      
-    }
-
-    public void Hide_Button(){     
-        button.enabled = false;
-        show = false;      
-    }
-    public void Hide_Quest_Mark(){     
-        quest_mark.enabled = false;
-        show = false;      
-    }
-
-    public void Show_Quest_Mark(){     
-        quest_mark.enabled = false;
-        show = false;      
-    }
-
-    void Update()
-    {
-
-        if (Accept)
-        {
-            player = GameObject.FindWithTag("Player");
-
-            for (int i = 0; i < questList.Count; i++) 
-            { 
-                float distance = Vector3.Distance(questList[i].transform.position, player.transform.position);
-
-                if (distance < proximityThreshold)
-                {
-                    taken++;
-                    Update_quest(taken, max);
-                    questList[i].enabled = false;
-                    Destroy(questList[i]);
-
-                    questList.RemoveAt(i);
-                }
-            }
-
-            if(max == taken && !finished)
-                Finish_quest();
-        }
-
-        if(cutscene_show && Input.anyKeyDown){
-            cutscene_show = false;
-            show_CutScene_text.gameObject.SetActive(false);
-        }
-    }
-
-    void Update_quest(int taken, int max){
-        string s = taken + "/" + max;
-        show_text.text = s;
-    }
-    void Show_items(){
-        for (int i = 0; i < questList.Count; i++) { 
-            questList[i].enabled = true;
-        }
-    }
-
-    public void Accept_quest(){
-        interact = false;
-        show_text.text = description;
-        Accept = true;
-
-        cutscene_Show();
-        Hide_Button();
-        Hide_Quest_Mark();
-        Show_items();
-    }
-
-    private void cutscene_Show(){
-        cutscene_show = true;
-        show_CutScene_text.text = cutscene;
-    }
-
-    
-
-    */
+        // Wait for the specified duration
+        yield return new WaitForSeconds(duration);
+        finished = true;
+        // Hide the text after the duration
+        show_CutScene_text.gameObject.SetActive(false);
+    }  
 }
